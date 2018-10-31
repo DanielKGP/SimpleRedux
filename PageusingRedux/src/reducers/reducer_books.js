1832fe19b292e297078ab03cabe0a19ed86590d3
ganpengkang <ganpeng@evestemptation.com>
2018-10-30 17:02:35 -0700

    books

---
+++ PageusingRedux/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return[
+        {title:'Javascript', pages:100},
+        {title:'harry',pages:10},
+        {title:'The dark',pages:50},
+        {title:'Ruby',pages:1}
+    ];
+}
